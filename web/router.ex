bf0c131ab5c25b0b609bc4e8e0f7ed678520934d
Steve Loveless <steve.loveless@gmail.com>
2016-03-07 10:33:50 -0800

    Done with creating users

--- web/router.ex
+++ web/router.ex
@@ -16,6 +16,7 @@ defmodule Rumbl.Router do
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
 
+    resources "/users", UserController, only: [:index, :show, :new, :create]
     get "/", PageController, :index
   end
 
